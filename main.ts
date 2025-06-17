5b93b248476f88c53bbd557c328c60b19cbbecde
Vítek Dolanský <vitek.dolansky.024@pslib.cz>
2025-06-17 22:36:22 +0200

    taky kompletni zmena a prechod jen na kod pro autopilot

--- main.ts
+++ main.ts
@@ -1,41 +1,69 @@
-radio.setGroup(23)
 
-let trim = 0
-let mode = "manual"
+type IRC = {
+    l: DigitalPin,
+    r: DigitalPin,
+    c: DigitalPin
+}
+const IR: IRC = {
+    l: DigitalPin.P14,
+    r: DigitalPin.P13,
+    c: DigitalPin.P15,
+}
+
+pins.setPull(IR.l, PinPullMode.PullNone);
+pins.setPull(IR.c, PinPullMode.PullNone);
+pins.setPull(IR.r, PinPullMode.PullNone);
 
 basic.forever(function () {
-    if (mode == "manual") {
-        let x = input.acceleration(Dimension.X)
-        let y = input.acceleration(Dimension.Y)
-        radio.sendString(`${x},${y}`)
-    }
-    basic.pause(50)
+    let left = pins.digitalReadPin(IR.l)
+    let right = pins.digitalReadPin(IR.r)
+    let center = pins.digitalReadPin(IR.c)
 })
 
-input.onButtonPressed(Button.AB, function () {
-    if (mode == "manual") {
-        mode = "auto"
-        radio.sendValue("mode", 1)
+function forward() {
+    PCAmotor.MotorRun(PCAmotor.Motors.M1, 175)
+    PCAmotor.MotorRun(PCAmotor.Motors.M4, 175)
+}
+function left() {
+    PCAmotor.MotorRun(PCAmotor.Motors.M1, 175)
+    PCAmotor.MotorRun(PCAmotor.Motors.M4, 125)
+}
+function right() {
+    PCAmotor.MotorRun(PCAmotor.Motors.M1, 125)
+    PCAmotor.MotorRun(PCAmotor.Motors.M4, 175)
+}
+function stop() {
+    PCAmotor.MotorStopAll()
+}
+
+basic.forever(function () {
+    let dataL = pins.digitalReadPin(IR.l)
+    let dataR = pins.digitalReadPin(IR.r)
+    let dataC = pins.digitalReadPin(IR.c)
+
+
+    if (dataC === 0 && dataL === 1 && dataR === 1) {
+        forward()
+    } else if (dataL === 0) {
+        left()
+    } else if (dataR === 0) {
+        right()
     } else {
-        mode = "manual"
-        radio.sendValue("mode", 0)
+        stop()
     }
-})
 
-input.onButtonPressed(Button.A, function () {
-    trim -= 5
-    radio.sendValue("trim", trim)
-    basic.showNumber(trim)
-})
 
-input.onButtonPressed(Button.B, function () {
-    trim += 5
-    radio.sendValue("trim", trim)
-    basic.showNumber(trim)
-})
+    input.onButtonPressed(Button.AB, function () {
+        basic.showNumber(dataC)
+    })
 
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    trim = 0
-    radio.sendValue("trim", trim)
-    basic.showNumber(trim)
+    input.onButtonPressed(Button.A, function () {
+        basic.showNumber(dataR)
+    })
+
+    input.onButtonPressed(Button.B, function () {
+        basic.showNumber(dataL)
+    })
+
+    basic.pause(20)
 })
