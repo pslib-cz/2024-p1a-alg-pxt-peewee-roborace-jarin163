53363515dfe06cae3dd5872dcd876b276e396dac
Vítek Dolanský <vitek.dolansky.024@pslib.cz>
2025-06-18 15:34:51 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -5,65 +5,82 @@ type IRC = {
     c: DigitalPin
 }
 const IR: IRC = {
-    l: DigitalPin.P14,
+    l: DigitalPin.P15,
     r: DigitalPin.P13,
-    c: DigitalPin.P15,
+    c: DigitalPin.P14,
 }
 
 pins.setPull(IR.l, PinPullMode.PullNone);
 pins.setPull(IR.c, PinPullMode.PullNone);
 pins.setPull(IR.r, PinPullMode.PullNone);
 
-basic.forever(function () {
-    let left = pins.digitalReadPin(IR.l)
-    let right = pins.digitalReadPin(IR.r)
-    let center = pins.digitalReadPin(IR.c)
-})
 
-function forward() {
-    PCAmotor.MotorRun(PCAmotor.Motors.M1, 175)
-    PCAmotor.MotorRun(PCAmotor.Motors.M4, 175)
-}
-function left() {
-    PCAmotor.MotorRun(PCAmotor.Motors.M1, 175)
-    PCAmotor.MotorRun(PCAmotor.Motors.M4, 125)
-}
-function right() {
-    PCAmotor.MotorRun(PCAmotor.Motors.M1, 125)
-    PCAmotor.MotorRun(PCAmotor.Motors.M4, 175)
-}
-function stop() {
-    PCAmotor.MotorStopAll()
-}
+
+//function forward() {
+//    PCAmotor.MotorRun(PCAmotor.Motors.M1, 200)
+//    PCAmotor.MotorRun(PCAmotor.Motors.M4, -200)
+//}
+//function left() {
+//    PCAmotor.MotorRun(PCAmotor.Motors.M1, 200)
+//    PCAmotor.MotorRun(PCAmotor.Motors.M4, -50)
+//}
+//function right() {
+//    PCAmotor.MotorRun(PCAmotor.Motors.M1, 50)
+//    PCAmotor.MotorRun(PCAmotor.Motors.M4, -200)
+//}
+
+
+//let last_side = "";
+
 
 basic.forever(function () {
     let dataL = pins.digitalReadPin(IR.l)
     let dataR = pins.digitalReadPin(IR.r)
     let dataC = pins.digitalReadPin(IR.c)
 
+    //console.log([dataL, dataC, dataR]);
+    //
+    //console.log(last_side)
+    //
+    //
+    //console.log(last_side)
+    //
+    if (dataC === 1) {
+        //forward()
+        //console.log("forward")
+        PCAmotor.MotorRun(PCAmotor.Motors.M1, 200)
+        PCAmotor.MotorRun(PCAmotor.Motors.M4, -200)
+        basic.pause(10)
 
-    if (dataC === 0 && dataL === 1 && dataR === 1) {
-        forward()
-    } else if (dataL === 0) {
-        left()
-    } else if (dataR === 0) {
-        right()
-    } else {
-        stop()
     }
+    //else 
+    if (dataL === 1) {
+        //left()
+        PCAmotor.MotorRun(PCAmotor.Motors.M1, 150)
+        PCAmotor.MotorRun(PCAmotor.Motors.M4, 100)
+        //last_side = "l"
+        //console.log("left")
+        basic.pause(10)
+    }
+    //else 
+    if (dataR === 1) {
+        //right()
+        PCAmotor.MotorRun(PCAmotor.Motors.M1, -100)
+        PCAmotor.MotorRun(PCAmotor.Motors.M4, -150)
+        //last_side = "r"
+        //console.log("right")
+        basic.pause(10)
+    } /*else {
+        
+        if (last_side === "r") {
+            PCAmotor.MotorRun(PCAmotor.Motors.M1, -100)
+            PCAmotor.MotorRun(PCAmotor.Motors.M4, -200)
+        }
+        else if (last_side === "l") {
+            PCAmotor.MotorRun(PCAmotor.Motors.M1, 200)
+            PCAmotor.MotorRun(PCAmotor.Motors.M4, 100)
+        }
+    } */
 
-
-    input.onButtonPressed(Button.AB, function () {
-        basic.showNumber(dataC)
-    })
-
-    input.onButtonPressed(Button.A, function () {
-        basic.showNumber(dataR)
-    })
-
-    input.onButtonPressed(Button.B, function () {
-        basic.showNumber(dataL)
-    })
-
-    basic.pause(20)
+    basic.pause(10)
 })
